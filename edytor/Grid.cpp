adf908be49b358822d5d6a13e00d3b9d0d612aba
Emanuel Gifuni <s194407@student.pg.edu.pl>
2023-12-31 13:49:21 +0100

    Added globals

--- edytor/Grid.cpp
+++ edytor/Grid.cpp
@@ -1,6 +1,6 @@
 #include "Grid.hpp"
 
-Grid::Grid()
+Grid::Grid() : position(100,100), scale(5, 5), totalCells(10,5), cellSize(16, 16),lineThickness(2) // defining the values in the constructor
 {
 }
 
@@ -14,38 +14,20 @@ void Grid::Initialize()
     // grid for 10 elements  
     // 16x16, 10x16=160
 
-    sf::Vector2f position = sf::Vector2f(100, 100);
+    int horizontalLineLength = totalCells.x * cellSize.x * scale.x; + lineThickness; // adding +lineThickness to fill the gap in the corner
+    int verticalLineLength = totalCells.y * cellSize.y * scale.y;
 
-    int scaleX = 5; // for the scale of the whole grid
-    int scaleY = 5;
-
-    int totalCellsX = 10; // total tiles on the X axis
-    int totalCellsY = 5; //  total tiles on the Y axis
-
-    int cellSizeX = 16; // a tile of 16px
-    int cellSizeY = 16; 
-    
-    int lineThickness = 2; 
-    int horizontalLineLength = totalCellsX * cellSizeX * scaleX;
-    int verticalLineLength = totalCellsY * cellSizeY * scaleY;
-
-    for(int i = 0; i < 5 + 1; i++)
+    for(int i = 0; i < totalLinesY; i++)
     {
         hLine[i].setSize(sf::Vector2f(horizontalLineLength, lineThickness));
-        hLine[i].setPosition(position + sf::Vector2f(0, i * cellSizeY * scaleY)); // draws a line every 16px on the Y axis
+        hLine[i].setPosition(position + sf::Vector2f(0, i * cellSize.y * scale.y)); // draws a line every 16px on the Y axis
     }
 
-     for(int i = 0; i < 10 + 1; i++)
+     for(int i = 0; i < totalLinesX; i++)
     {
         vLine[i].setSize(sf::Vector2f(lineThickness, verticalLineLength));
-        vLine[i].setPosition(position + sf::Vector2f(i * cellSizeX * scaleX, 0)); // draws a line every 16px on the X axis
+        vLine[i].setPosition(position + sf::Vector2f(i * cellSize.x * scale.x, 0)); // draws a line every 16px on the X axis
     }
-
-//     hLine.setSize(sf::Vector2f(horizontalLineLength, lineThickness));
-//     hLine.setPosition(position);
-
-//     vLine.setSize(sf::Vector2f(lineThickness, verticalLineLength));
-//     vLine.setPosition(position);
 }
 
 void Grid::Load()
@@ -61,15 +43,14 @@ void Grid::Update(double deltaTime)
 void Grid::Draw(sf::RenderWindow &window)
 {
 
-    for(int i = 0; i < 5 + 1; i++) // + 1 in the iterator for the extreme bottom and extreme right lines to appear
+    for(int i = 0; i < totalLinesY; i++) // + 1 in the iterator for the extreme bottom and extreme right lines to appear
     {
         window.draw(hLine[i]); // to draw the horizontal line
     }
 
-    for(int i = 0; i < 10 + 1; i++)
+    for(int i = 0; i < totalLinesX; i++)
     {
         window.draw(vLine[i]); // to draw the vertical line
     }
-//     window.draw(hLine); // to draw the horizontal line
-//     window.draw(vLine); // to draw the vertical line
+
 }
--- edytor/Grid.hpp
+++ edytor/Grid.hpp
@@ -1,19 +1,30 @@
 #pragma once 
 #include <SFML/Graphics.hpp>
 
+#define totalLinesX 10 + 1
+#define totalLinesY 5 + 1
+
+
 class Grid 
 {
 private: 
     sf::RectangleShape hLine[5 + 1]; // array of horizontal lines in the grid
     sf::RectangleShape vLine[10 + 1]; // array of vertical lines in the grid
 
+    sf::Vector2f position;
+    sf::Vector2i scale; // for the scale of the whole grid
+    sf::Vector2i totalCells; // total tiles on the X and Y axis
+    sf::Vector2i cellSize; // a tile of 16px
+     
+    int lineThickness; 
+
 public:
     Grid();
     ~Grid();
 
-    void Initialize(); //wywoływane raz
-    void Load(); //wywoływane raz
+    void Initialize(); 
+    void Load(); 
 
-    void Update(double deltaTime); //wywoływane raz na klatkę
-    void Draw(sf::RenderWindow& window); //wywoływane raz na klatkę
+    void Update(double deltaTime); 
+    void Draw(sf::RenderWindow& window); 
 };
