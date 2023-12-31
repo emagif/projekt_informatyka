e1c99b42d169681867a16043e5a218c0254f4e14
Emanuel Gifuni <s194407@student.pg.edu.pl>
2023-12-31 15:50:36 +0100

    Added comments and changed passing to function

--- edytor/Grid.cpp
+++ edytor/Grid.cpp
@@ -1,12 +1,17 @@
 #include "Grid.hpp"
 
-Grid::Grid() : position(100,100), scale(5, 5), totalCells(10,5), cellSize(16, 16),lineThickness(2) // defining the values in the constructor
+Grid::Grid(const sf::Vector2i& totalCells) : m_position(100,100), m_scale(5, 5), m_totalCells(totalCells), m_cellSize(16, 16), m_lineThickness(2) // defining the values in the constructor
 {
+    m_totalLines = sf::Vector2i(totalCells.x + 1,totalCells.y + 1); // amount of total lines +1 in every dimension
+
+    m_hLine = new sf::RectangleShape[m_totalLines.y];
+    m_vLine = new sf::RectangleShape[m_totalLines.x];
 }
 
 Grid::~Grid()
 {
-
+    delete[] m_hLine;
+    delete[] m_vLine;
 }
 
 void Grid::Initialize()
@@ -14,19 +19,19 @@ void Grid::Initialize()
     // grid for 10 elements  
     // 16x16, 10x16=160
 
-    int horizontalLineLength = totalCells.x * cellSize.x * scale.x; + lineThickness; // adding +lineThickness to fill the gap in the corner
-    int verticalLineLength = totalCells.y * cellSize.y * scale.y;
+    int horizontalLineLength = m_totalCells.x * m_cellSize.x * m_scale.x;
+    int verticalLineLength = m_totalCells.y * m_cellSize.y * m_scale.y + m_lineThickness;   // adding +lineThickness to fill the gap in the corner
 
-    for(int i = 0; i < totalLinesY; i++)
+    for(int i = 0; i < m_totalLines.y; i++)
     {
-        hLine[i].setSize(sf::Vector2f(horizontalLineLength, lineThickness));
-        hLine[i].setPosition(position + sf::Vector2f(0, i * cellSize.y * scale.y)); // draws a line every 16px on the Y axis
+        m_hLine[i].setSize(sf::Vector2f(horizontalLineLength, m_lineThickness));
+        m_hLine[i].setPosition(m_position + sf::Vector2f(0, i * m_cellSize.y * m_scale.y)); // draws a line every 16px on the Y axis
     }
 
-     for(int i = 0; i < totalLinesX; i++)
+     for(int i = 0; i < m_totalLines.x; i++)
     {
-        vLine[i].setSize(sf::Vector2f(lineThickness, verticalLineLength));
-        vLine[i].setPosition(position + sf::Vector2f(i * cellSize.x * scale.x, 0)); // draws a line every 16px on the X axis
+        m_vLine[i].setSize(sf::Vector2f(m_lineThickness, verticalLineLength));
+        m_vLine[i].setPosition(m_position + sf::Vector2f(i * m_cellSize.x * m_scale.x, 0)); // draws a line every 16px on the X axis
     }
 }
 
@@ -43,14 +48,14 @@ void Grid::Update(double deltaTime)
 void Grid::Draw(sf::RenderWindow &window)
 {
 
-    for(int i = 0; i < totalLinesY; i++) // + 1 in the iterator for the extreme bottom and extreme right lines to appear
+    for(int i = 0; i < m_totalLines.y; i++)    // + 1 in the iterator for the extreme bottom and extreme right lines to appear
     {
-        window.draw(hLine[i]); // to draw the horizontal line
+        window.draw(m_hLine[i]);              // to draw the horizontal line
     }
 
-    for(int i = 0; i < totalLinesX; i++)
+    for(int i = 0; i < m_totalLines.x; i++)
     {
-        window.draw(vLine[i]); // to draw the vertical line
+        window.draw(m_vLine[i]);              // to draw the vertical line
     }
 
 }
--- edytor/Grid.hpp
+++ edytor/Grid.hpp
@@ -1,25 +1,24 @@
 #pragma once 
 #include <SFML/Graphics.hpp>
 
-#define totalLinesX 10 + 1
-#define totalLinesY 5 + 1
-
-
 class Grid 
 {
 private: 
-    sf::RectangleShape hLine[5 + 1]; // array of horizontal lines in the grid
-    sf::RectangleShape vLine[10 + 1]; // array of vertical lines in the grid
+    sf::RectangleShape* m_hLine; // array of horizontal lines in the grid
+    sf::RectangleShape* m_vLine; // array of vertical lines in the grid
 
-    sf::Vector2f position;
-    sf::Vector2i scale; // for the scale of the whole grid
-    sf::Vector2i totalCells; // total tiles on the X and Y axis
-    sf::Vector2i cellSize; // a tile of 16px
+    sf::Vector2f m_position;
+    sf::Vector2i m_scale;         // for the scale of the whole grid
+    sf::Vector2i m_totalCells;    // total tiles on the X and Y axis
+    sf::Vector2i m_cellSize;      // a tile of 16px
+
+    sf::Vector2i m_totalLines;    // total lines on the X and Y axis
+
      
-    int lineThickness; 
+    int m_lineThickness; 
 
 public:
-    Grid();
+    Grid(const sf::Vector2i& totalCells);
     ~Grid();
 
     void Initialize(); 
