6fbc0d3e6d3a0f5cb131e9bd2a1d7e3b0ec3bf9a
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-01-04 23:40:46 +0100

    Changed nothing

--- DrawRectangles.hpp
+++ DrawRectangles.hpp
@@ -4,9 +4,9 @@
 class DrawRectangles
 {
 private:
-    std::vector<sf::RectangleShape> m_rectangles;
 
 public:     
+    std::vector<sf::RectangleShape> m_rectangles;
     DrawRectangles();
     void addRectangle(float x, float y, float sizeX, float sizeY, sf::Color color); 
     void Draw(sf::RenderWindow& window);
