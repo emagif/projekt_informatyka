2d13b17828d7c2a83fe3d4b7a99dc1bdc0f62b8f
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-01-05 13:07:24 +0100

    Changed the collision algorithm, now it is working

--- CheckCollision.cpp
+++ CheckCollision.cpp
@@ -7,14 +7,50 @@ CheckCollision::CheckCollision(std::vector<sf::RectangleShape>& rectangles) : m_
 
 void CheckCollision::checkCollision(const sf::RectangleShape& rectangle, double deltaTime, Player1& player)
 {
+    sf::FloatRect rectBounds = rectangle.getGlobalBounds();
+    sf::Vector2f playerPos = player.m_sprite.getPosition();
     for (const auto& otherRect : m_rectangles)
     {
-        if(rectangle.getGlobalBounds().intersects(otherRect.getGlobalBounds()))
-        {
+        sf::FloatRect otherBounds = otherRect.getGlobalBounds();
 
-            sf::Vector2f position = player.m_sprite.getPosition();
-            std::cout<<"True"<<std::endl;
-            player.m_sprite.setPosition(position + sf::Vector2f(-3,0));
+        if(rectBounds.intersects(otherBounds))
+        {
+            float overlapLeft = otherBounds.left + otherBounds.width - rectBounds.left;
+            float overlapRight = rectBounds.left + rectBounds.width - otherBounds.left;
+            float overlapTop = otherBounds.top + otherBounds.height - rectBounds.top;
+            float overlapBottom = rectBounds.top + rectBounds.height - otherBounds.top;
+
+            bool fromLeft = overlapRight < overlapLeft;
+            bool fromTop = overlapBottom < overlapTop;
+
+            float minOverlapX = std::min(overlapLeft, overlapRight);
+            float minOverlapY = std::min(overlapTop, overlapBottom);
+
+            if(minOverlapX < minOverlapY)
+            {
+                if(fromLeft) // checks for collision on the X axis
+                {
+                    playerPos.x -= minOverlapX;
+                }
+                else
+                {
+                    playerPos.x += minOverlapX;
+                }
+            }
+
+            else
+            {
+                if(fromTop) // checks for collision on the Y axis
+                {
+                    playerPos.y -= minOverlapY;
+                }
+
+                else
+                {
+                    playerPos.y +=minOverlapY;
+                }
+            }
         }
     }
+    player.m_sprite.setPosition(playerPos);
 }
