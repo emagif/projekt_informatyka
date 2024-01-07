02acdf1b68a76db4f2bb9a1bf83fe7e9ec152388
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-01-07 09:21:39 +0100

    Tried to add saving to and reading from file

---
+++ SavingAndReadingFromFile.cpp
@@ -0,0 +1,13 @@
+#include "SavingAndReadingFromFile.hpp"
+
+SavingAndReadingFromFile::SavingAndReadingFromFile()
+{
+}
+
+void SavingAndReadingFromFile::SaveToFile()
+{
+}
+
+void SavingAndReadingFromFile::ReadFromFile()
+{
+}
